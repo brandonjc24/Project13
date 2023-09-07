11e1aad14905593b804065f9f7085af41a8f162f
Brandon Coles <brandonjc24@gmail.com>
2023-09-07 13:42:15 -0400

    Added test to create tabs from DSP_Options array

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,6 +9,25 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+static juce::String getDSPOptionName(Project13AudioProcessor::DSP_Option option)
+{
+    switch (option)
+    {
+    case Project13AudioProcessor::DSP_Option::Phase:
+        return "PHASE";
+    case Project13AudioProcessor::DSP_Option::Chorus:
+        return "CHORUS";
+    case Project13AudioProcessor::DSP_Option::OverDrive:
+        return "OVERDRIVE";
+    case Project13AudioProcessor::DSP_Option::LadderFilter:
+        return "LADDERFILTER";
+    case Project13AudioProcessor::DSP_Option::GeneralFilter:
+        return "GEN FILTER";
+    case Project13AudioProcessor::DSP_Option::END_OF_LIST:
+        jassertfalse;
+    }
+    return "NO SELECTION";
+}
 //==============================================================================
 Project13AudioProcessorEditor::Project13AudioProcessorEditor (Project13AudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
@@ -25,10 +44,14 @@ Project13AudioProcessorEditor::Project13AudioProcessorEditor (Project13AudioProc
 
         auto range = juce::Range<int>(static_cast<int>(Project13AudioProcessor::DSP_Option::Phase),
             static_cast<int>(Project13AudioProcessor::DSP_Option::END_OF_LIST));
+        tabbedComponent.clearTabs();
+
         for (auto& v : dspOrder)
         {
             auto entry = r.nextInt(range);
             v = static_cast<Project13AudioProcessor::DSP_Option>(entry);
+
+            tabbedComponent.addTab(getDSPOptionName(v), juce::Colours::lightslategrey, -1);
         }
 
         DBG(juce::Base64::toBase64(dspOrder.data(), dspOrder.size()));
@@ -38,7 +61,8 @@ Project13AudioProcessorEditor::Project13AudioProcessorEditor (Project13AudioProc
         jassertfalse;
     };
     addAndMakeVisible(dspOrderButton);
-
+    addAndMakeVisible(dspOrderButton);
+    addAndMakeVisible(tabbedComponent);
 }
 
 Project13AudioProcessorEditor::~Project13AudioProcessorEditor()
@@ -58,5 +82,8 @@ void Project13AudioProcessorEditor::paint (juce::Graphics& g)
 
 void Project13AudioProcessorEditor::resized()
 {
-    dspOrderButton.setBounds(getLocalBounds().reduced(100));
+    auto bounds = getLocalBounds();
+    dspOrderButton.setBounds(bounds.removeFromTop(30).withSizeKeepingCentre(150, 30));
+    bounds.removeFromTop(10);
+    tabbedComponent.setBounds(bounds.withHeight(30));
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -24,11 +24,31 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+
+    /*
+     https://forum.juce.com/t/draggabletabbedcomponent/13265/5?u=matkatmusic
+     "You can create a subclass of TabbedButtonBar which is also a DragAndDropTarget. And you can create custom tab buttons which allow themselves to be dragged."
+     */
+    struct ExtendedTabbedButtonBar : juce::TabbedButtonBar, juce::DragAndDropTarget
+    {
+        ExtendedTabbedButtonBar() : juce::TabbedButtonBar(juce::TabbedButtonBar::Orientation::TabsAtTop) { }
+
+        bool isInterestedInDragSource(const SourceDetails& dragSourceDetails) override { return false; }
+        void itemDropped(const SourceDetails& dragSourceDetails) override { }
+    };
+
+    struct ExtendedTabBarButton : juce::TabBarButton
+    {
+
+    };
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Project13AudioProcessor& audioProcessor;
-
     juce::TextButton dspOrderButton{ "dsp order" };
+
+    //    juce::TabbedComponent tabbedComponent { juce::TabbedButtonBar::Orientation::TabsAtTop };
+    ExtendedTabbedButtonBar tabbedComponent;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Project13AudioProcessorEditor)
 };
